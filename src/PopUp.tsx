5ae60d55112f56c6719dc07dd818a9991edc8c72
Dipak Khade <dipakhade214@gmail.com>
2025-08-03 13:08:16 +0530

    content ts added

--- src/PopUp.tsx
+++ src/PopUp.tsx
@@ -4,11 +4,18 @@ import { useEffect } from 'react'
 function Popup() {
   useEffect(() => {
     console.log('hello there')
+    const handleKeyPress = (e: KeyboardEvent) => {
+      if (e.key === 'd') {
+        chrome.tabs.create({ url: 'https://example.com' }) // 👈 opens new tab
+      }
+    }
+
+    window.addEventListener('keydown', handleKeyPress)
+    return () => window.removeEventListener('keydown', handleKeyPress)
   }, [])
   return (
     <div className='text-red-400 bg-green-300'>
       hello from this extension
-
     </div>
   );
 }

47a96ce83474aa31f459f12c367bdfe0b5a1b1ff
Dipak Khade <dipakhade214@gmail.com>
2025-08-13 18:04:55 +0530

    added tabs

--- src/PopUp.tsx
+++ src/PopUp.tsx
@@ -14,13 +14,26 @@ function Popup() {
     <TabsContext value={[
       { title: 'Completed', action: () => { console.log('complted action') }, isActive: false },
       { title: 'Add', action: () => { console.log('add action') }, isActive: false },
+      { title: 'Incomplete', action: () => { console.log('Incomplete action') }, isActive: true },
     ]}>
       <TodoContext value={[{ id: 1, completed: false, date: new Date, desc: 'this is a todo desc', title: 'title is this' }
       ]}>
         <Wrapper>
           <>
             <Header />
-            {mode === 'View' ? <ViewTodo /> : <AddTodo />}
+            {
+
+              tabs.find(x => x.title === 'Completed')[0]['isActive'] === true && <div>asd</div>
+
+            }
+            {
+              tabs.find(x => x.title === 'Add')[0]['isActive'] === true && <AddTodo />
+
+            }
+            {
+
+              tabs.find(x => x.title === 'Incomplete')[0]['isActive'] === true && <ViewTodo />
+            }
             <Tabs />
           </>
         </Wrapper>

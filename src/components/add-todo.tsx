712e64671c498dfb80c2941621317abed355c3c1
Dipak Khade <dipakhade214@gmail.com>
2025-08-13 11:15:02 +0530

    add todo changes

--- src/components/add-todo.tsx
+++ src/components/add-todo.tsx
@@ -1,7 +1,10 @@
 
 export default function AddTodo() {
   return <>
-
+    <div>
+      <input type="text" placeholder="Enter Title" />
+      <input type="text" placeholder="Enter Description" />
+    </div>
 
   </>
 }

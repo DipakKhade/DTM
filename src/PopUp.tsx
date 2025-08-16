cf7902feac4b1940a69ca1de50a2b8076c7679ee
Dipak Khade <dipakhade214@gmail.com>
2025-08-16 12:57:57 +0530

    checkpoint

--- src/PopUp.tsx
+++ src/PopUp.tsx
@@ -5,8 +5,10 @@ import Header from './components/header';
 import { TodoContext } from './context/todo-context';
 import { MdAssignmentAdd } from "react-icons/md";
 import { TiTick } from "react-icons/ti";
+import AddTodo from './components/add-todo';
+import ViewTodo from './components/view-todo';
 function Popup() {
-  const [_tab, SetTab] = useState<'Add' | 'ViewCompleted' | 'ViewIncomplted'>('ViewIncomplted');
+  const [tab, SetTab] = useState<'Add' | 'ViewCompleted' | 'ViewIncomplted'>('ViewIncomplted');
 
   return (
     <TodoContext value={[{ id: 1, completed: false, date: new Date, desc: 'this is a todo desc', title: 'title is this' }
@@ -15,22 +17,24 @@ function Popup() {
         <>
           <Header />
 
-          <div className="w-full h-12 bg-[rgba(0, 0, 0, 0.87)] text-[#a5a5a5] fixed bottom-0 border border-top border-slate-700">
-            <div className="flex item-center justify-around">
+          {tab === 'ViewIncomplted' && <ViewTodo />}
+          {tab === 'Add' && <AddTodo />}
+          {tab === 'ViewCompleted' && <div>asd</div>}
 
-              <button className="cursor-pointer text-2xl item-center justify-between bold space-x-4 rounded-sm" onClick={() => SetTab('Add')}>
-                <MdAssignmentAdd />
-              </button>
-              <button className="cursor-pointer text-2xl item-center justify-between bold space-x-4 rounded-sm" onClick={() => SetTab('Add')}>
-                < TiTick />
-              </button>
-              <button className="cursor-pointer text-2xl item-center justify-between bold space-x-4 rounded-sm" onClick={() => SetTab('Add')}>
-                <MdAssignmentAdd />
-              </button>
+          <div>
+            <div className="w-full h-12 bg-[rgba(0, 0, 0, 0.87)] text-[#a5a5a5] fixed bottom-0 border border-top border-slate-700">
+              <div className="flex item-center justify-around">
 
+                <button className="cursor-pointer text-2xl item-center justify-between bold space-x-4 rounded-sm" onClick={() => SetTab('Add')}>
+                  <MdAssignmentAdd />
+                </button>
+                <button className="cursor-pointer text-2xl item-center justify-between bold space-x-4 rounded-sm" onClick={() => SetTab('ViewCompleted')}>
+                  < TiTick />
+                </button>
+              </div>
             </div>
-          </div>
 
+          </div>
         </>
       </Wrapper>
     </TodoContext>

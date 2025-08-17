9b885733aac725e7d3177d303963966c3259ee0b
Dipak Khade <dipakhade214@gmail.com>
2025-08-17 22:38:47 +0530

    added padding

--- src/PopUp.tsx
+++ src/PopUp.tsx
@@ -25,7 +25,7 @@ function Popup() {
             <div className="w-full h-12 bg-[rgba(0, 0, 0, 0.87)] text-[#a5a5a5] fixed bottom-0 border border-top border-slate-700">
               <div className="flex item-center justify-around">
 
-                <button className="cursor-pointer text-2xl item-center justify-between bold space-x-4 rounded-sm" onClick={() => SetTab('Add')}>
+                <button className="pt-2 cursor-pointer text-2xl item-center justify-between bold space-x-4 rounded-sm" onClick={() => SetTab('Add')}>
                   <MdAssignmentAdd />
                 </button>
                 <button className="cursor-pointer text-2xl item-center justify-between bold space-x-4 rounded-sm" onClick={() => SetTab('ViewCompleted')}>
